2740003c59470d76d90c231338e37fbc442da1bc
AlbertKovtoun <kovtoun.albert@hotmail.com>
2024-12-08 15:42:28 +0100

    Commit of shame

--- 2/main.js
+++ 2/main.js
@@ -1,3 +1,5 @@
+// THIS IS NOT CORRECT WITH PART 2
+
 import { input } from "./input.js"
 
 const inputArray = []
@@ -12,15 +14,12 @@ let safeReports = 0
 for (let i = 0; i < inputArray.length; i++) {
   const firstLevel = inputArray[i][0]
   const secondLevel = inputArray[i][1]
-
   let amountPassed = 0
-
   firstLevel < secondLevel ? (isAscending = true) : (isAscending = false)
 
-  for (let j = 0; j < inputArray[i].length; j++) {
+  for (let j = 0; j < inputArray[i].length - 1; j++) {
     const currentLevel = inputArray[i][j]
     const nextLevel = inputArray[i][j + 1]
-
     if (isAscending) {
       if (nextLevel >= currentLevel + 1 && nextLevel <= currentLevel + 3) {
         amountPassed++
@@ -32,8 +31,34 @@ for (let i = 0; i < inputArray.length; i++) {
     }
   }
 
-  if (amountPassed + 1 === inputArray[i].length) {
+  if (amountPassed === inputArray[i].length - 1) {
     safeReports++
+    continue
+  }
+
+  for (let k = 0; k < inputArray[i].length; k++) {
+    let inputArrayCopy = [...inputArray[i]]
+    inputArrayCopy.splice(k, 1)
+    amountPassed = 0
+
+    for (let l = 0; l < inputArrayCopy.length - 1; l++) {
+      const currentLevel = inputArrayCopy[l]
+      const nextLevel = inputArrayCopy[l + 1]
+      if (isAscending) {
+        if (nextLevel >= currentLevel + 1 && nextLevel <= currentLevel + 3) {
+          amountPassed++
+        }
+      } else if (!isAscending) {
+        if (nextLevel <= currentLevel - 1 && nextLevel >= currentLevel - 3) {
+          amountPassed++
+        }
+      }
+    }
+
+    if (amountPassed === inputArrayCopy.length - 1) {
+      safeReports++
+      continue
+    }
   }
 }
 
